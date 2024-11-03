d53152d9ebdbcd6ed60668ad7f8130f48a90eb05
Gabriel Freitas <freitasga.01@gmail.com>
2024-11-03 19:59:05 -0300

    format files and remove unused file

--- Assets/Scripts/MeshData.cs
+++ Assets/Scripts/MeshData.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 
 public class MeshData
 {
-    public List<Vector3> Vertices;
-    public List<int> Triangles;
+    public readonly List<Vector3> Vertices;
+    public readonly List<int> Triangles;
 
     public MeshData(List<Vector3> vertices, List<int> triangles)
     {
--- Assets/Scripts/Point.cs
+++ Assets/Scripts/Point.cs
@@ -1,15 +1,14 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Point
 {
     public Vector3 Position;
-    public List<Point> Edges;
+    public readonly List<Point> Edges;
 
     public Point(Vector3 position)
     {
-        this.Position = position;
+        Position = position;
         Edges = new List<Point>();
     }
 }
